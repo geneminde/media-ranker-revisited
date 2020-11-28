d226d6f97047ca0fec777562801e1b407346123e
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-28 14:43:47 -0800

    add github login path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-ruby # config/initializers/omniauth.rb
+# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_CLIENT_ID"],
            ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
